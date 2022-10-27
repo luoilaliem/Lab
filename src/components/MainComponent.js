2be21b569958f3759bd766bfe1282c4a2ea6aacc
Xuan Loc <Xuanloc1302@gmail.com>
2022-10-28 03:08:44 +0700

    Single Page Applications Part 2

--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Contact from "./ContactComponent"; 
+import Contact from "./ContactComponent";
 import Menu from "./MenuComponent";
 import { COMMENTS } from '../shared/comments';
 import { PROMOTIONS } from '../shared/promotions';
@@ -7,13 +7,14 @@ import { LEADERS } from '../shared/leaders';
 import { DISHES } from "../shared/dishes";
 import Header from "./HeaderComponent";
 import Footer from "./FooterComponent";
-import { BrowserRouter,Route, Switch, Redirect } from "react-router-dom";
+import { BrowserRouter, Route, Switch, Redirect } from "react-router-dom";
 import Home from "./HomeComponent";
+import DishDetail from "./DishdetailComponent";
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      dishes: DISHES,      
+      dishes: DISHES,
       comments: COMMENTS,
       promotions: PROMOTIONS,
       leaders: LEADERS
@@ -25,28 +26,34 @@ class Main extends Component {
   }
 
   render() {
-   
+    const DishWithId = ({ match }) => {
+      return (
+        <DishDetail dish={this.state.dishes.filter((dish) => dish.id === parseInt(match.params.dishId, 10))[0]}
+          comments={this.state.comments.filter((comment) => comment.dishId === parseInt(match.params.dishId, 10))} />
+      );
+    };
     const HomePage = () => {
-      return(
-        <Home 
-        dish={this.state.dishes.filter((dish) => dish.featured)[0]}
-        promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
-        leader={this.state.leaders.filter((leader) => leader.featured)[0]}
-          />
+      return (
+        <Home
+          dish={this.state.dishes.filter((dish) => dish.featured)[0]}
+          promotion={this.state.promotions.filter((promo) => promo.featured)[0]}
+          leader={this.state.leaders.filter((leader) => leader.featured)[0]}
+        />
       );
     }
-    
+
     return (
       <div>
         <Header />
         <Switch>
-              <Route path='/home' component={HomePage} />
-              <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
-              <Route exact path='/contactus' component={Contact} />
-              <Redirect to="/home" />
-          </Switch>
-        
-        <Footer/>
+          <Route path='/menu/:dishId' component={DishWithId} />
+          <Route path='/home' component={HomePage} />
+          <Route exact path='/menu' component={() => <Menu dishes={this.state.dishes} />} />
+          <Route exact path='/contactus' component={Contact} />
+          <Redirect to="/home" />
+        </Switch>
+
+        <Footer />
       </div>
     );
   }
